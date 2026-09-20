7c884c2812d33f0ff9f35614e8caebcea2b2a2f9
ayntharasan <ayntharasana@gmail.com>
2026-09-20 20:07:10 +0530

    Update SCRIPT_URL to new Google Apps Script URL

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ⚠️ STEP 1: Paste your Google Apps Script Web App URL here after deploying it.
 // It looks like: https://script.google.com/macros/s/XXXXXXXXXXXXXXXX/exec
-const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbwqGvdylTIrfmF9iOxFoi6mvVwxu4uNO4UmM0vdoGn0xkd1n0mNS8jM_dz40g6UvzSV/exec";
+const SCRIPT_URL = "https://script.google.com/macros/s/AKfycbw6mGsVHU5oAwMN58_TOQScXqp4nkWPF2YIen66dsEbzQFBdmrR36ukpcjxiRbuILdx/exec";
 
 const form = document.getElementById("userForm");
 const statusMsg = document.getElementById("statusMsg");
